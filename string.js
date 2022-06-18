e5006c343b8c275faf3925d1160c6731e147ca28
Muhammad Usman Naeem <76055167+usman-naeem728@users.noreply.github.com>
2022-06-18 12:13:22 +0500

    Add files via upload

---
+++ string.js
@@ -0,0 +1,55 @@
+// document.write(`<h1>question 1</h1> </br>`)
+
+// var firstName = prompt("enter first name");
+// var lastName = prompt("enter last name");
+// var fullName = document.write(`welcome ${firstName} ${lastName} </br>`)
+
+// document.write(`<h1>question 2</h1> </br>`)
+// var inp = prompt("enter your favorite phone");
+// document.write(`my favorite phone is:  ${inp} </br> length of string is ${inp.length} </br>`)
+
+// document.write(`<h1>question 3</h1> </br>`)
+// var letter1 = "Pakistan";
+// document.write(`index number of n is ${letter1.indexOf("n")} in ${letter1} </br>`)
+
+// document.write(`<h1>question 4</h1> </br>`)
+// var letter2 = "Hello world";
+// document.write(`index number of l is ${letter2.lastIndexOf("l")} in ${letter2} </br>`)
+
+// document.write(`<h1>question 5</h1> </br>`)
+// var letter3 = "Pakistan";
+// document.write(`index number of i is ${letter3.indexOf("i")} in ${letter3} </br>`)
+
+// document.write(`<h1>question 6</h1> </br>`)
+// var firstName1 = prompt("enter first name ");
+// var lastName1 = prompt("enter last name");
+// var fullName = document.write(`welcome ${firstName1.concat(lastName1)} </br>`)
+
+// document.write(`<h1>question 7</h1> </br>`)
+// var city = "hyderabad";
+// document.write(`City: ${city} </br> After replacement: islam${city.slice(5)} </br>`)
+
+// document.write(`<h1>question 8</h1> </br>`)
+// var message = "Ali and Sami are best friends They play cricket and football together.";
+// document.write(`Message: ${message}</br> Afetr replacement: ${message.replace(/and/g,"&")}`)
+
+// document.write(`<h1>question 9</h1> </br>`)
+// var str = "472";
+// var num = Number(str);
+// document.write(`Value: ${str} </br>  Type:${typeof(str)} </br> Value: ${num} </br> Type: ${typeof(num)} </br> `)
+
+// document.write(`<h1>question 10</h1> </br>`)
+// var website = prompt("enter a url like www.example.com");
+// document.write(`url: ${website} </br> domain: ${website.slice(4)} </br>`)
+
+// document.write(`<h1>question 11</h1> </br>`)
+// var cap = prompt("enter any word");
+// document.write(`input: ${cap} </br> capital: ${cap.toUpperCase()} </br>`)
+
+// document.write(`<h1>question 12</h1> </br>`)
+// var low = prompt("enter any word");
+// document.write(`input: ${low} </br> lowercase: ${low.toLowerCase()} </br>`)
+
+// document.write(`<h1>question 13</h1> </br>`)
+// var cap1 = prompt("enter any word");
+// document.write(`input: ${cap1} </br> title case: ${cap1.charAt(0).toUpperCase().concat(cap1.slice(1))} </br>`)
